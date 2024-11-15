c113a0836ae93de1f25fc6537d9c209689d1ba4d
ToaFromSiberia <143086731+ToaFromSiberia@users.noreply.github.com>
2024-11-15 17:46:34 +0300

    Add files via upload

---
+++ server/client/ui/ClientView.java
@@ -0,0 +1,7 @@
+package server.client.ui;
+import server.client.domain.ClientController;
+public interface ClientView {
+    void showMessage(String message);
+    void disconnectedFromServer();
+    void setClientController(ClientController clientController);
+}
---
+++ server/server/repository/Repository.java
@@ -0,0 +1,5 @@
+package server.server.repository;
+public interface Repository<T> {
+    void save(T text);
+    T load();
+}
